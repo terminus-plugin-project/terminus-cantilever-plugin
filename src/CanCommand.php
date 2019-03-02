2843d8fe2d372cfb734c0601a6ab4d24d5fc4dda
Ed Reel <edreel@gmail.com>
2019-03-02 16:44:01 -0600

    Update for Terminus 2.x

--- src/CanCommand.php
+++ src/CanCommand.php
@@ -1,111 +1,172 @@
 <?php
 namespace Pantheon\TerminusCantilever;
 
-use Pantheon\Terminus\Commands\Site\SiteCommand;
+use Pantheon\Terminus\Collections\Sites;
+use Pantheon\Terminus\Commands\TerminusCommand;
+use Pantheon\Terminus\Exceptions\TerminusException;
+use Pantheon\Terminus\Site\SiteAwareInterface;
+use Pantheon\Terminus\Site\SiteAwareTrait;
 
-class CanCommand extends SiteCommand
+class CanCommand extends TerminusCommand implements SiteAwareInterface
 {
-	/**
-	 * Grab all sites.
-	 *
-	 * @authorize
-	 *
-	 * @command can
-	 *
-	 * @option env Choose site environment
-	 * @option level Choose site service level (free,basic,pro,business,performance)
-	 * @option frame Choose site framework (drupal,drupal8,wordpress)
-	 * @option tags Choose site tags (development,enterprise,terminated)
-	 * @option org Name of organization/membership site belongs to
-	 * @option command Add your command (use [site] for name.env, [name] for site name, and [env] for environment)
-	 *
-	 * terminus can --env=live --level='pro,business,performance' --frame='drupal,drupal8' --command='terminus drush [site] pml|grep redis'
-	 * terminus can --env=live --frame='wordpress' --command='terminus wp [site] option get home'
-	 *
-	 */
-	public function cantilever($options = ['env' => 'dev', 'level' => null, 'frame' => 'drupal', 'tags' => null, 'org' => null, 'command' => null])
-	{
-		//show initialize
-		$this->log()->notice("Cantilever initializing...");
+    use SiteAwareTrait;
 
-		//list sites
-		$sites = $this->sites->serialize();
+    /**
+     * Grab all sites.
+     *
+     * @authorize
+     *
+     * @command can
+     *
+     * @option env Choose site environment (dev,test,live,multidev)
+     * @option plan Choose site plan (sandbox,basic,performance small,performance medium,elite)
+     * @option frame Choose site framework (drupal,drupal8,wordpress)
+     * @option tags Choose site tags (development,enterprise,terminated)
+     * @option org Name of organization/membership site belongs to
+     * @option command Add your command (use [site] for name.env, [name] for site name, and [env] for environment)
+     *
+     * terminus can --env=live --plan='basic,performance small,performance medium,elite' --frame='drupal,drupal8' --command='terminus drush [site] pml|grep redis'
+     * terminus can --env=live --frame='wordpress' --command='terminus wp [site] option get home'
+     *
+     */
+    public function cantilever($options = ['env' => 'dev', 'plan' => null, 'frame' => 'drupal', 'tags' => null, 'org' => null, 'command' => null])
+    {
+        $sites_exist = false;
 
-		if (empty($sites)) {
-			$this->log()->notice('You have no sites.');
-		} else {
-			if (isset($options['env'])) {
-				echo "environment: " . $options['env']."\n";
-			}
-			if (isset($options['tags'])) {
-				echo "tags: " . $options['tags']."\n";
-			}
-			if (isset($options['org'])) {
-				echo "organization: " . $options['org']."\n";
-			}
-			if (isset($options['env']) || isset($options['tags']) || isset($options['org'])) {
-				echo "----------\n\n";
-			}
+        //show initialize
+        $this->log()->notice("Cantilever initializing...");
 
-			//site loop
-			foreach ($sites as $key => $site) {
-//            	print_r($site);
+        //list sites
+        $sites = $this->sites->serialize();
 
-				//remove non selected sites
-				if (isset($options['level'])) {
-					$level = explode(",", $options['level']);
-					if (!in_array($site['service_level'], $level, true)) {
-						unset($sites[$key]);
-					}
-				}
+        if (empty($sites)) {
+            $this->log()->notice('You have no sites.');
+            exit;
+        }
+        if (isset($options['env'])) {
+            echo "environment: " . $options['env'] . "\n";
+        } else {
+            echo "environment: dev\n";
+        }
+        if (isset($options['plan'])) {
+            echo "plan: " . $options['plan']."\n";
+        }
+        if (isset($options['frame'])) {
+            echo "framework: " . $options['frame'] . "\n";
+        } else {
+            echo "framework: drupal\n";
+        }
+        if (isset($options['tags'])) {
+            echo "tags: " . $options['tags']."\n";
+        }
+        if (isset($options['org'])) {
+            echo "organization: " . $options['org'] . "\n";
+        }
+        if (isset($options['command'])) {
+            echo "command: " . $options['command'] . "\n";
+        }
 
-				if (isset($options['frame'])) {
-					$frame = explode(",", $options['frame']);
-					if (!in_array($site['framework'], $frame, true)) {
-						unset($sites[$key]);
-					}
-				}
+        //site loop
+        foreach ($sites as $key => $site) {
 
-				if (isset($options['tags'])) {
-					$tags = explode(",", $options['tags']);
-					if (!isset($site['tags']) || (isset($site['tags']) && !in_array($site['tags'], $tags, true))) {
-						unset($sites[$key]);
-					}
-				}
+            //select available environments
+            $all_envs = [];
+            $envs = isset($options['env']) ? explode(',', $options['env']) : ['dev'];
+            if ($environments = $this->getSite($site['name'])->getEnvironments()->serialize()) {
+                foreach ($environments as $environment) {
+                    $env = $environment['id'];
+                    $all_envs[] = $env;
+                    if (!$environment['initialized'] && ($index = array_search($env, $envs)) !== false) {
+                        unset($envs[$index]);
+                    }
+                }
+            } else {
+                $envs = [];
+            }
 
-				if (isset($options['org'])) {
-					if (preg_match("/(\b" . $options['org'] . "\b)(\n|,|$)/", $site['memberships']) == false) {
-						unset($sites[$key]);
-					}
-				}
+            //remove unknown environments
+            if (isset($envs)) {
+                foreach ($envs as $env) {
+                    if (!in_array($env, $all_envs) && ($index = array_search($env, $envs)) !== false) {
+                        unset($envs[$index]);
+                    }
+                }
+            }
 
-				//run
-				if (empty($sites)) {
-					$this->log()->notice('You have no sites.');
-				} else {
-					if (isset($sites[$key])) {
-						//print site
-						echo $site['name'] . "\n";
+            //remove sites with no environments
+            if (!isset($envs)) {
+                unset($sites[$key]);
+            }
 
-						//compile command
-						if (isset($options['command'])) {
-							$command = $options['command'];
-							$command = str_replace("[site]", $site['name'] . "." . $options['env'], $command);
-							$command = str_replace("[name]", $site['name'], $command);
-							$command = str_replace("[env]", $options['env'], $command);
+            //remove non selected sites
+            if (isset($options['plan'])) {
+                $plan = explode(',', strtolower(trim($options['plan'])));
+                if (!in_array(strtolower($site['plan_name']), $plan)) {
+                    unset($sites[$key]);
+                }
+            }
 
-							echo "----------\n";
-							$output = shell_exec($command);
-							if ($output == '') {
-								$output = "** no results **\n";
-							}
+            if (isset($options['frame'])) {
+                $frame = explode(',', strtolower(trim($options['frame'])));
+                if (!in_array(strtolower($site['framework']), $frame)) {
+                    unset($sites[$key]);
+                }
+            }
 
-							//print output
-							echo $output . "\n";
-						}
-					}
-				}
-			}
-		}
-	}
+            if (isset($options['tags'])) {
+                $tags = explode(',', strtolower(trim($options['tags'])));
+                if (!isset($site['tags']) || (isset($site['tags']) && !in_array(strtolower($site['tags']), $tags))) {
+                    unset($sites[$key]);
+                }
+            }
+
+            if (isset($options['org'])) {
+                if (preg_match("/(\b" . $options['org'] . "\b)(\n|,|$)/", $site['memberships']) == false) {
+                    unset($sites[$key]);
+                }
+            }
+
+            //run
+            if (isset($sites[$key])) {
+                //compile command
+                if (isset($options['command'])) {
+                    foreach ($envs as $env) {
+                        //print site.env
+                        $site_env = $site['name'] . '.' . $env;
+                        $chars = strlen($site_env);
+                        echo str_repeat('=', $chars) . "\n";
+                        echo $site_env . "\n";
+                        echo str_repeat('=', $chars) . "\n";
+                        $command = $options['command'];
+                        $command = str_replace("[site]", $site_env, $command);
+                        $command = str_replace("[name]", $site['name'], $command);
+                        $command = str_replace("[env]", $env, $command);
+                        $output = $this->execute($command);
+                        echo $output . "\n";
+                        $sites_exist = true;
+                    }
+                }
+            }
+        }
+        if (!$sites_exist) {
+            $this->log()->notice('You have no sites that match this filter criteria.');
+        }
+    }
+
+    /**
+     * Executes the command.
+     */
+    protected function execute($cmd)
+    {
+        $process = proc_open(
+            $cmd,
+            [
+                0 => STDIN,
+                1 => STDOUT,
+                2 => STDERR,
+            ],
+            $pipes
+        );
+        proc_close($process);
+    }
 }
